269c13951a7ccb1afb4d2828160357452ebf5729
Matthew Duffy <mattduffy@gmail.com>
2025-07-26 22:10:25 -0700

    Figured out the correct format of the client configuaration options object to connect via createSentinel().

--- src/daos/impl/redis/redis-client.js
+++ src/daos/impl/redis/redis-client.js
@@ -1,13 +1,15 @@
 /**
  * @module @mattduffy/koa-stub
  * @author Matthew Duffy <mattduffy@gmail.com>
- * @file src/daos/imple/redis/redis-client.js The low-level connection object of redis.
+ * @summary The low-level connection object of redis - using the official node-redis package.
+ * @file src/daos/imple/redis/redis-client.js
  */
 
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
-import { Redis } from 'ioredis'
+// import { Redis } from 'ioredis'
+import { createSentinel } from 'redis'
 import * as Dotenv from 'dotenv'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -16,51 +18,64 @@ const root = path.resolve(`${__dirname}/../../../..`)
 console.log('redis-client.js >>root = ', root)
 const showDebug = process.env.NODE_ENV !== 'production'
 const redisEnv = {}
-Dotenv.config({ path: path.resolve(root, 'config/redis.env'), processEnv: redisEnv, debug: showDebug })
+Dotenv.config({
+  path: path.resolve(root, 'config/redis.env'),
+  processEnv: redisEnv,
+  debug: showDebug,
+})
 
+let tracer = []
 const sentinelPort = redisEnv.REDIS_SENTINEL_PORT ?? 26379
 const redisConnOpts = {
-  sentinels: [
+  sentinelRootNodes: [
     { host: redisEnv.REDIS_SENTINEL_01, port: sentinelPort },
     { host: redisEnv.REDIS_SENTINEL_02, port: sentinelPort },
     { host: redisEnv.REDIS_SENTINEL_03, port: sentinelPort },
   ],
   name: 'myprimary',
-  db: redisEnv.REDIS_DB,
-  keyPrefix: `${redisEnv.REDIS_KEY_PREFIX}:` ?? 'koa:',
-  sentinelUsername: redisEnv.REDIS_SENTINEL_USER,
-  sentinelPassword: redisEnv.REDIS_SENTINEL_PASSWORD,
-  username: redisEnv.REDIS_USER,
-  password: redisEnv.REDIS_PASSWORD,
-  connectionName: 'ioredis',
-  enableTLSForSentinelMode: true,
-  sentinelRetryStrategy: 100,
-  tls: {
-    ca: await fs.readFile(redisEnv.REDIS_CACERT),
-    rejectUnauthorized: false,
-    requestCert: true,
+  database: redisEnv.REDIS_DB,
+  sentinelClientOptions: {
+    username: redisEnv.REDIS_SENTINEL_USER,
+    password: redisEnv.REDIS_SENTINEL_PASSWORD,
+    socket: {
+      tls: true,
+      rejectUnauthorized: false,
+      ca: await fs.readFile(redisEnv.REDIS_CACERT),
+    },
   },
-  sentinelTLS: {
-    ca: await fs.readFile(redisEnv.REDIS_CACERT),
-    rejectUnauthorized: false,
-    requestCert: true,
+  nodeClientOptions: {       
+    username: redisEnv.REDIS_USER,
+    password: redisEnv.REDIS_PASSWORD,
+    socket: {
+      tls: true,
+      rejectUnauthorized: false,
+      ca: await fs.readFile(redisEnv.REDIS_CACERT),
+    },
   },
-  showFriendlyErrorStack: true,
-  retryStrategy(times) {
-    const delay = Math.min(times * 50, 2000)
-    return delay
-  },
-  /* eslint-disable consistent-return */
-  reconnectOnError(err) {
-    const targetError = 'closed'
-    if (err.message.includes(targetError)) {
-      return true
-    }
-    // return false
-  },
+  sentinelRetryDelayOnFailover: 100,
+  maxRetriesPerRequest: 3,
+  lazyConnect: true,
+  role: 'master',
 }
 console.log(redisConnOpts)
-const redis = new Redis(redisConnOpts)
+let sentinel
+try {
+  sentinel = await createSentinel(redisConnOpts)
+    .on("reconnecting", () => {
+      console.log("Redis sentinel reconnecting");
+    })
+    .on('error', err => console.error('Redis Sentinel Error', err))
+    .on("ready", () => {
+      console.log("Redis sentinel connection is ready");
+    })
+
+    sentinel.setTracer(tracer)
+    await sentinel.connect()
+  // const redis = sentinel.master
+} catch (e) {
+  console.log(e)
+  console.log('tracer:', tracer)
+}
 export {
-  redis,
+  sentinel as redis,
 }
