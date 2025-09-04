ec902e466fcc07ccb3ef6235c835b32dbc70e35f
Matthew Duffy <mattduffy@gmail.com>
2025-09-04 09:21:17 -0700

    Remove last lingering references to ioredis in the session-handler middleware file.

--- src/session-handler.js
+++ src/session-handler.js
@@ -31,52 +31,6 @@ dotenv.config({
 // console.log('cacert: %o', process.env.REDIS_CACERT)
 
 const sentinelPort = redisEnv.REDIS_SENTINEL_PORT || 26379
-const ioredisConnOpts = {
-  sentinels: [
-    { host: redisEnv.REDIS_SENTINEL_01, port: sentinelPort },
-    { host: redisEnv.REDIS_SENTINEL_02, port: sentinelPort },
-    { host: redisEnv.REDIS_SENTINEL_03, port: sentinelPort },
-  ],
-  name: 'myprimary',
-  db: redisEnv.REDIS_DB,
-  keyPrefix: `${redisEnv.REDIS_KEY_PREFIX}:sessions:` ?? 'koa:sessions:',
-  sentinelUsername: redisEnv.REDIS_SENTINEL_USER,
-  sentinelPassword: redisEnv.REDIS_SENTINEL_PASSWORD,
-  username: redisEnv.REDIS_USER,
-  password: redisEnv.REDIS_PASSWORD,
-  connectionName: `${redisEnv.REDIS_CONNECTION_NAME}-sessions`,
-  enableTLSForSentinelMode: true,
-  showFriendlyErrorStack: true,
-  keepAlive: 10000,
-  tls: {
-    ca: fs.readFileSync(redisEnv.REDIS_CACERT),
-    rejectUnauthorized: false,
-    requestCert: true,
-  },
-  sentinelTLS: {
-    ca: fs.readFileSync(redisEnv.REDIS_CACERT),
-    rejectUnauthorized: false,
-    requestCert: true,
-  },
-  retryStrategy(times) {
-    const delay = Math.min(times * 50, 2000)
-    return delay
-  },
-  // sentinelRetryStrategy: 100,
-  sentinelRetryStrategy(times) {
-    const delay = Math.min(times * 50, 2000)
-    return delay
-  },
-  /* eslint-disable consistent-return */
-  reconnectOnError(err) {
-    const targetError = 'closed'
-    if (err.message.includes(targetError)) {
-      return true
-    }
-    // return false
-  },
-}
-// const ioredis = redisStore(ioredisConnOpts)
 const redisConnOpts = {
   isRedisReplset: true,
   keyPrefix: `${redisEnv.REDIS_KEY_PREFIX}:sessions:` ?? 'koa:sessions:',
@@ -110,14 +64,12 @@ const redisConnOpts = {
   lazyConnect: true,
   role: 'master',
 }
-// const redis = redisStore(ioredisConnOpts)
 const redis = await redisStore.init(redisConnOpts)
 console.log(
   'did redisStore init work?', await redis.ping()
 )
 
 const config = {
-  // store: ioredis,
   store: redis,
   key: redisEnv.SESSION_KEY ?? 'session',
   maxAge: redisEnv.SESSION_1_DAY * 2 ?? (86400000 * 2),
@@ -135,5 +87,4 @@ export {
   session,
   config,
   redis,
-  // ioredis,
 }
